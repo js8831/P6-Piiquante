2fdfacc00149f71927064c27d659269140d65987
Jean Soulié <kalashh7@gmail.com>
2022-09-23 16:01:08 +0200

    feat : add routes

---
+++ routes/sauce.js
@@ -0,0 +1,18 @@
+// Import d'express pour créer des routeurs avec sa méthode
+const express = require("express");
+// Appel du routeur
+const router = express.router();
+
+//  get
+router.get();
+router.get();
+
+// post
+router.post();
+router.post();
+
+// put
+router.put();
+
+// delete
+router.delete();
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+// Import d'express pour créer des routeurs avec sa méthode
+const express = require("express");
+// Appel du routeur
+const router = express.router();
+
+// post
+router.post();
+router.post();
