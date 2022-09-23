88eda113956baf335ad4de51c5ae0473ef017d63
Jean Soulié <kalashh7@gmail.com>
2022-09-23 16:01:21 +0200

    feat : add controllers

---
+++ controllers/sauce.js
@@ -0,0 +1,3 @@
+// Importation du modele sauce pour l'utiliser dans les middlewares qu'on affectera aux routes
+// La majuscule a-t-elle un impact ? Comme je l'ai nommé Sauce
+const Sauce = require("../models/sauce");
