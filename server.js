1f93eb6e35bfeaad5553ac202014336380680cad
Jean Soulié <kalashh7@gmail.com>
2022-10-04 16:35:37 +0200

    fix : set server

--- server.js
+++ server.js
@@ -4,12 +4,53 @@ const http = require("http");
 // Importation de l'application express depuis app.js
 const app = require("./app");
 
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+// Il faut dire à l'app express sur quel port, elle va tourner (on set le port : l'environnement ou 3000)
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 // Création du serveur qui recevra les req http via app
 // Notre serveur retourne donc notre app express
 const server = http.createServer(app);
 
-// Port sur lequel va ecouter notre serveur pour chaque requete envoyé du frontend. 3000 ou var. env. si indisponibilité
-server.listen(process.env.PORT || 3000);
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
 
-// Il faut dire à l'app express sur quel port, elle va tourner (on set le port : l'environnement ou 3000)
-app.set("port", process.env.PORT || 3000);
+// Port sur lequel va ecouter notre serveur pour chaque requete envoyé du frontend. 3000 ou var. env. si indisponibilité
+server.listen(port);
